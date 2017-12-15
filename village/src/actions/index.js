2bb4ba53d9958cedd9d10cb0a2a60fa795d05539
Dresean  Williams <dreseanwilliams@Dreseans-MacBook-Pro.local>
2017-12-15 18:29:25 -0500

    get and post completed

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -2,11 +2,32 @@ import axios from 'axios';
 
 export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
+export const DELETE_SMURF = "DELETE_SMURF";
 
 export const getSmurfs = () => {
-
+  const apiUrl = 'http://localhost:3333/';
+ const promise = axios.get(apiUrl);
+  return {
+    type: GET_SMURFS,
+    payload: promise
+  }
 }
 
 export const addSmurf = (smurf) => {
+  const apiUrl = 'http://localhost:3333/smurfs';
+  const promise = axios.post(apiUrl, smurf);
+   return {
+     type: ADD_SMURF,
+     payload: promise
+   }
+}
 
+export const deleteSmurf = (id) => {
+ const apiUrl = 'http://localhost:333/smurfs';
+ const promise = axios.delete(apiUrl, id);
+ return {
+  id: {
+
+  }
+ }
 }
